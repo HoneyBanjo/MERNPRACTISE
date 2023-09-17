c1a2b288c343d56b31452d4e04866dc8701b2491
Benjamin Nousiainen <b.nousiainen@gmail.com>
2023-09-17 23:15:07 +0300

    Conditional routing based on user existing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { useAuthContext } from './hooks/useAuthContext';
 
 
 // pages & components
@@ -10,6 +10,8 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 function App() {
+  const {user} = useAuthContext()
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -18,15 +20,15 @@ function App() {
           <Routes>
             <Route
             path="/"
-            element={<Home />}
+            element={user ? <Home /> : <Navigate to="/login" />}
             />
             <Route
             path="/login"
-            element={<Login />}
+            element={!user ? <Login />: <Navigate to="/" />}
             />
             <Route
             path="/signup"
-            element={<Signup />}
+            element={!user ? <Signup /> : <Navigate to="/" />}
             />
           </Routes>
         </div>
